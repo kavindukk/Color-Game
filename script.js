795b7f884fc0a534aabb91ab4d5dccb6547522a1
Kavindu <z.kavindu@gmail.com>
2019-04-07 21:31:00 +0530

    commit-1

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
-function rgbgen(){
-    return Math.round(Math.random()*255);
+var colors=[];
+var colorboxes = document.querySelectorAll(".colorbox");
+var DisplayColor = document.querySelector("#displaycolor");
+var h1=document.querySelector("h1");
+
+// var h1= document.getElementsByTagName("h1");
+ColorArryGenerator(6);
+var RandomColor = RandomColorPicker();
+
+//Assign Colors Array to Box Background Collor
+for (var i=0; i<colorboxes.length; i++){
+    colorboxes[i].style.background=colors[i];
 }
 
-var arr = [];
-var colors = [];
-for (i=0; i<9; i++){
-    arr[i]=[];
-    for (j=0; j<3; j++){
-        arr[i][j]=rgbgen();
-    };
+for(var i=0; i<colorboxes.length; i++){
+    colorboxes[i].addEventListener("click",function(){
+        if(this.style.background===RandomColor){
+            for(var j=0; j<colorboxes.length; j++){
+                colorboxes[j].style.background=RandomColor;
+            }
+            h1.style.background=RandomColor;            
+        }
+        else{
+            this.style.background="black";
+        }
+        
+    });
+}
 
-    colors[i]='RGB('+arr[i][0]+','+arr[i][1]+','+arr[i][2]+')';
-    
-};
+//Assign Random Color to Colors Array
+function ColorArryGenerator(num){
+    for(var i=0; i<num; i++){
+        colors.push(RandomColorGenerator());
+    }
+}
 
-document.querySelectorAll(".row .box")[0].style.backgroundColor="blue";
+//Generate Random Color
+function RandomColorGenerator(){
+    //Random r picker
+    var r = Math.floor(Math.random()*256);
+    //Random g picker
+    var g = Math.floor(Math.random()*256);
+    //Random b picker
+    var b = Math.floor(Math.random()*256);
 
+    return "rgb("+r+", "+g+", "+b+")"    
+}
 
-
-
-
+//Random Color Picker From Colors Array
+function RandomColorPicker(){
+    var i =Math.floor(Math.random()*6);
+    DisplayColor.textContent=colors[i];
+    return colors[i];
+}
