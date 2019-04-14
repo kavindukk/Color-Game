6a0fc4695813f8eaa608b82ee5a4be547804d27f
Kavindu <z.kavindu@gmail.com>
2019-04-14 19:39:57 +0530

    commit 2

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ var colors=[];
 var colorboxes = document.querySelectorAll(".colorbox");
 var DisplayColor = document.querySelector("#displaycolor");
 var h1=document.querySelector("h1");
+var textarea = document.querySelector("#result");
+var ResetButton = document.getElementById("reset");
+var EasyButton = document.getElementById("easy");
+var HardButton = document.getAnimations("hard");
 
-// var h1= document.getElementsByTagName("h1");
 ColorArryGenerator(6);
 var RandomColor = RandomColorPicker();
 
@@ -12,20 +15,37 @@ for (var i=0; i<colorboxes.length; i++){
     colorboxes[i].style.background=colors[i];
 }
 
+//ColorBox Click Event
 for(var i=0; i<colorboxes.length; i++){
     colorboxes[i].addEventListener("click",function(){
         if(this.style.background===RandomColor){
             for(var j=0; j<colorboxes.length; j++){
                 colorboxes[j].style.background=RandomColor;
             }
-            h1.style.background=RandomColor;            
+            h1.style.background=RandomColor; 
+            textarea.textContent = "Congatulation The Selection is Correct..";           
         }
         else{
             this.style.background="black";
+            textarea.textContent = "Try Again...!!!";
         }
         
     });
 }
+
+// Try Again Button Click Event
+ResetButton.addEventListener("click", function(){
+    colors=[];
+    ColorArryGenerator(6);
+    RandomColor = RandomColorPicker();
+    h1.style.background = "black";
+    textarea.textContent="Select the Mentioned Color Correctly.";
+
+    //Assign Colors Array to Box Background Color
+    for (var i=0; i<colorboxes.length; i++){
+        colorboxes[i].style.background=colors[i];
+    }
+});
 
 //Assign Random Color to Colors Array
 function ColorArryGenerator(num){
